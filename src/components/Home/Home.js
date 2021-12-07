e9740b754c6a615d6c5b051b725cdbe24ebaa2c1
musasazib <musasazib@gmail.com>
2021-12-07 13:40:46 +0600

    Add resume section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Resume from '../Resume/Resume';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Resume></Resume>
         </div>
     );
 };
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Resume.css'
+
+const Resume = () => {
+    return (
+        <div className="container">
+            <h1 className="hi">Hi_</h1>
+            <p className="about-description">I am Sazib, a Junior MERN Stack Web developer able to build a Web presence from the ground up - from concept, navigation, layout and programming. Experienced with a demonstrated history of working in the computer software industry. Skilled in Front End technologies such as Html5 , Css3 , JavaScript , React , BootStrap, Material UI, and Back End technologies such as Node.JS, Express JS and MongoDb. Learning a variety of scripting languages and multimedia tools is one of my strengths. I am also a hard worker and team player. I am looking for a challenging opportunity that will enable me to use my skills and abilities to achieve a challenging goal.</p>
+            <a href="https://drive.google.com/file/d/19S2CWuMt5FDZoT1BaT4oUJt3C-xJQd_b/view?usp=sharing" target="blank" className="resume-btn">Download Resume</a>
+        </div>
+        
+    );
+};
+
+export default Resume;
