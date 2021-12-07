d0771be2ee6c8ddc430f1af2869863ced2b4416a
musasazib <musasazib@gmail.com>
2021-12-07 18:56:24 +0600

    Add projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -76,19 +76,61 @@ const Projects = () => {
                         </ul>
                         <ul className="tags">
                             <li>#React JS</li>
-                            <li>#Node JS</li>
-                            <li>#Express JS</li>
-                            <li>#MongoDB</li>
                             <li>#Firebase</li>
-                            <li>#Heroku</li>
+                            <li>#Netlify</li>
                             <li>#React Bootstrap</li>
                             <li>#React Router</li>
                             <li>#React Hook Form</li>
                         </ul>
                         <div className="project-link">
-                            <a href="https://matley-headphone.web.app/" target="blank">Live Site</a>
-                            <a href="https://github.com/musasazib/matley-headphone-client" target="blank"> Client Side</a>
-                            <a href="https://github.com/musasazib/matley-headphone-server" target="blank"> Server Side</a>
+                            <a href="https://fit-lab-dhaka.web.app/" target="blank">Live Site</a>
+                            <a href="https://github.com/musasazib/fit-lab" target="blank"> Client Side</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 pt-5 mt-5">
+                        <img src="https://i.ibb.co/zPV19kc/aments-Project.png" alt="" class="img-fluid pb-5 aos-init" />
+                    </div>
+                    <div class="col-md-8 mt-5">
+                        <h3 class="mt-md-0 mt-sm-5">❖ Learn Up</h3>
+                        <p>Nov 2021 - Nov 2021</p>
+                        <ul className="my-item">
+                            <li>➣ A fully functional dynamic responsive website.</li>
+                            <li>➣ Users can log in with email & password or their google account.</li>
+                            <li>➣ World class learning academy. Good quality all courses come here.</li>
+                        </ul>
+                        <ul className="tags">
+                            <li>#React JS</li>
+                            <li>#Firebase</li>
+                            <li>#Netlify</li>
+                            <li>#React Bootstrap</li>
+                            <li>#React Router</li>
+                            <li>#React Hook Form</li>
+                        </ul>
+                        <div className="project-link">
+                            <a href="https://learn-up.netlify.app/" target="blank">Live Site</a>
+                            <a href="https://github.com/musasazib/learn-up" target="blank"> Client Side</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 pt-5 mt-5">
+                        <img src="https://i.ibb.co/zPV19kc/aments-Project.png" alt="" class="img-fluid pb-5 aos-init" />
+                    </div>
+                    <div class="col-md-8 mt-5">
+                        <h3 class="mt-md-0 mt-sm-5">❖ The Best Programming Team In The World</h3>
+                        <p>Nov 2021 - Nov 2021</p>
+                        <ul className="my-item">
+                            <li>➣ A fully functional dynamic responsive website.</li>
+                            <li>➣ Users can log in with email & password or their google account.</li>
+                            <li>➣ After login they can choose their needed medical service and get an appointment.</li>
+                        </ul>
+                        <ul className="tags">
+                            <li>#React JS</li>
+                            <li>#Bootstrap</li>
+                            <li>#Netlify</li>
+                            
+                        </ul>
+                        <div className="project-link">
+                            <a href="https://world-class-developers.netlify.app/" target="blank">Live Site</a>
+                            <a href="https://github.com/musasazib/programming-team" target="blank"> Client Side</a>
                         </div>
                     </div>
                 </div>
